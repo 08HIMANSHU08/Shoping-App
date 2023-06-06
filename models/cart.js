cdeb6dc3f3a8f3119683dd540dc52229f8bbc6bf
08HIMANSHU08 <himanshusinghpatel@gmail.com>
2023-06-06 15:14:47 +0530

    Models file

---
+++ models/cart.js
@@ -0,0 +1,16 @@
+
+
+const Sequelize = require('sequelize');
+
+const sequelize = require('../util/database');
+
+const Cart =  sequelize.define('cart',{
+    id:{
+        type:Sequelize.INTEGER,
+        autoIncrement:true,
+        allouNull: false,
+        primaryKey: true
+    }
+});
+
+module.exports = Cart;
